7c0c944438174d44fa8be71f67e2a8865101b53e
shasanudin <shidiqhasanudin6@gmail.com>
2026-09-06 00:18:13 +0700

    Set up Firebase configuration and Firestore
    Initialize Firebase app and Firestore database.

---
+++ js/firebase.js
@@ -0,0 +1,34 @@
+import {
+initializeApp
+}
+from 
+"https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+
+
+import {
+getFirestore
+}
+from
+"https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+
+
+const firebaseConfig={
+
+  apiKey: "AIzaSyAhQvRHBYX7dGW7QiSVN24cukmYHrN6d1c",
+  authDomain: "data-ke-aksi-auth.firebaseapp.com",
+  projectId: "data-ke-aksi-auth",
+  storageBucket: "data-ke-aksi-auth.firebasestorage.app",
+  messagingSenderId: "631382692174",
+  appId: "1:631382692174:web:c10a099fb3021849eace1f",
+  measurementId: "G-9NMBBB79EQ"
+};
+
+
+
+const app=
+initializeApp(firebaseConfig);
+
+
+export const db=
+getFirestore(app);
